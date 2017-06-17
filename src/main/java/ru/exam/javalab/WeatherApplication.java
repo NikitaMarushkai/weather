615fef0bfea435b6c48779c4a8d273c70b631506
n.marushkai <unlimited54321@mail.ru>
2017-06-17 09:35:04 +0300

    initial

---
+++ src/main/java/ru/exam/javalab/WeatherApplication.java
@@ -0,0 +1,12 @@
+package ru.exam.javalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApplication.class, args);
+    }
+}
